1ff3bba1ad6f0d687388e7fc066dace1bd26c11b
paultron <palark93@gmail.com>
2019-05-08 14:46:17 -0600

    Signed-off-by: paultron <palark93@gmail.com>

--- Python-Frames.py
+++ Python-Frames.py
@@ -6,11 +6,11 @@ start = open('C:\DEV\Python-Frames\start.txt', 'r')
 start1 = start.read()
 start.close()
 
-mida = open('C:\DEV\Python-Frames\mid-1st.txt', 'r')
-mid1 = mida.read()
+#mida = open('C:\DEV\Python-Frames\mid-1st.txt', 'r')
+#mid1 = mida.read()
 #mida.close()
 
-midb = open('C:\DEV\Python-Frames\mid-2nd.txt', 'r')
+#midb = open('C:\DEV\Python-Frames\mid-2nd.txt', 'r')
 #mid1 = midb.read()
 #midb.close()
 
@@ -18,11 +18,17 @@ end = open('C:\DEV\Python-Frames\end.txt', 'r')
 end1 = end.read()
 end.close()
 
-frames = 10
+times = 0 #frame counter
 
-duration = 1000
+oldNum = 660 #<id> counter
 
-color = '#ffffffff'
+fps = 30
+
+duration = 1000 #not implemented, in ms, 1sec = 1000
+
+frames = int(duration/1000) * fps #fits frames to length
+
+color = '#ffffffff' #not implemented
 
 #open fresh .cueprofile
 new = open('C:\DEV\Python-Frames\profilenew.cueprofile', 'w+')
@@ -31,30 +37,47 @@ new.write(start1)
 #change to append mode
 new = open('C:\DEV\Python-Frames\profilenew.cueprofile', 'a')
 # write the code for inserting the frames
-oldNum = 660
-new.write('\n' + mid1 + '\n')
+
+mida = open('C:\DEV\Python-Frames\mid-1st.txt', 'r')
+##new.write('\n' + mida + '\n')
+with mida as f:
+    new.write('\n')
+    for line in f:
+        if '<time>' in line:
+            new.write(('\t'*32) + '<time>' + str(times/fps) + '</time>\n')
+            times += 1
+        else:
+            new.write(line)
+    new.write('\n')
+    
 for fr in range(frames-1):
     if oldNum == 660:
         new.write(('\t'*23) + '<value' + str(fr +1) + '>\n')
+        #omits the newline for the first new frame
     else:
         new.write('\n' + ('\t'*23) + '<value' + str(fr +1) + '>\n')
-    
+        
+    times -= 1 # ensures 0-1, 1-2, etc
     midb = open('C:\DEV\Python-Frames\mid-2nd.txt', 'r')
     with midb as f:
-        flip = True
         for line in f:
             if '<id>21' in line:
                 nid = list(line)
                 nid[-9:-6] = list(str(oldNum))
                 new.write(''.join(nid))
                 oldNum += 1
+            elif '<time>' in line:
+                new.write(('\t'*32) + '<time>' + str(times/fps) + '</time>\n')
+                times += 1                
             else:
                 new.write(line)
 
     new.write('\n' + ('\t'*23) + '</value' + str(fr +1) + '>')
+    
 ###append the rest of the profile
-###adjusting the <id> numbers along the way
 new.write('\n' + end1)
 
 #close the file
+mida.close()
+midb.close()
 new.close()
